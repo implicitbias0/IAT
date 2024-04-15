277f2688d6264ef32cfe5fefef50bda7b5cb95a5
implicitbias0 <163656527+implicitbias0@users.noreply.github.com>
2024-04-15 20:14:16 +0200

    Update raceiat.js

--- raceiat.js
+++ raceiat.js
@@ -16,7 +16,7 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
                 {word: 'Jessica'},
                 {word: 'Maria'},
                 {word: 'Nora'},                 
-                {image: 'Sophia'}     
+                {word: 'Sophia'}     
             ],
             //Stimulus css (style)
             stimulusCss : {color:'#31940F','font-size':'2.3em'}
@@ -29,12 +29,12 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
                 height : 4 //Used to position the "Or" in the combined block.
             }, 
             stimulusMedia : [ //Stimuli content as PIP's media objects
-                {image: 'wm1_nc.jpg'},
-                {image: 'wm2_nc.jpg'},
-                {image: 'wm3_nc.jpg'},
-                {image: 'wf1_nc.jpg'},
-                {image: 'wf2_nc.jpg'},
-                {image: 'wf3_nc.jpg'}
+                {word: 'Christian'},
+                {word: 'David'},
+                {word: 'Henrik'},
+                {word: 'Peter'},
+                {word: 'Oliver'},
+                {word: 'Thomas'}
             ],
             //Stimulus css (style)
             stimulusCss : {color:'#31940F','font-size':'2.3em'}
