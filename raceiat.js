3b539028e2d0a98b07990a8fb12464689bb64268
implicitbias0 <163656527+implicitbias0@users.noreply.github.com>
2024-04-15 20:44:51 +0200

    Update raceiat.js

--- raceiat.js
+++ raceiat.js
@@ -6,7 +6,7 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
         category1 : {
             name : global.blackLabels, //Will appear in the data.
             title : {
-                media : {word : 'Female Names'}, //Name of the category presented in the task.
+                media : {word : global.blackLabels}, //Name of the category presented in the task.
                 css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
                 height : 4 //Used to position the "Or" in the combined block.
             }, 
